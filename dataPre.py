7f5e169e0614f27bba0484615212d8ecd310ad56
Daisy <daisyap2005@gmail.com>
2026-04-19 17:05:59 -0400

    Add files via upload

--- dataPre.py
+++ dataPre.py
@@ -1,39 +1,141 @@
 # Data Collection, Preprocessing, and Merging
 # Using multiple datasets, creating a merged dataset of university data across PA
 # Goal of project: classify best university for a user based on inputs
-
+import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.impute import SimpleImputer
+from sklearn.compose import ColumnTransformer
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
 
 class Data:
     def collect(self):
-        data1 = pd.read_csv('data/Supply_Demand_Gap_Analysis_2014-2024_State_System_of_Higher_Education_20260403.csv') # https://data.pa.gov/Post-Secondary-Education/Supply-Demand-Gap-Analysis-2014-2024-State-System-/n3sx-ckrr/about_data
-        # data2 may not work super well with the other data, but we can get some info from it
-        data2 = pd.read_csv('data/PA_Higher_Education_Institutions_by_Sector_Current_Statewide_Education_20260404.csv') # https://data.pa.gov/Post-Secondary-Education/PA-Higher-Education-Institutions-by-Sector-Current/8u5h-xvus/about_data
-        # Have to still research -> data3 = https://data.pa.gov/Education/Education-Names-and-Addresses-EdNA-Post-Secondary-/dtvt-jb9p/about_data
-        print(data1.head())
-        print(data2.head())
+        higherEdBySector = pd.read_csv('data/HigherEd_BySector.csv')
+        higherEdBySector.name = "HigherEducationBySector"
 
-        return data1, data2
+        higherEdInstitutions = pd.read_csv('data/HigherEd_Insitutions.csv')
+        higherEdInstitutions = higherEdInstitutions[higherEdInstitutions["County"] != "Out-of-State"]
+        higherEdInstitutions.name = "HigherEducationInstitutions"
+        #print(higherEdInstitutions['Category'].unique())
+
+        cost = pd.read_csv('data/Data_4-19-2026---701.csv')
+        cost.name = "Cost"
+
+        """ PASSHE Data
+        totalHeadcount = pd.read_csv('data/passhe_data/Total_Headcount.csv', encoding='utf-16', sep='\t')
+        totalHeadcount.name = "TotalHeadcount"
+
+        fullTime = pd.read_csv('data/passhe_data/FullTime.csv', encoding='utf-16', sep='\t')
+        fullTime.name = "FullTime"
+
+        livingOnCampus = pd.read_csv('data/passhe_data/LivingOnCampus.csv', encoding='utf-16', sep='\t')
+        livingOnCampus.name = "LivingOnCampus"
+
+        adultLearner = pd.read_csv('data/passhe_data/AdultLearner.csv', encoding='utf-16', sep='\t')
+        adultLearner.name = "AdultLearner"
+
+        educationMajor = pd.read_csv('data/passhe_data/EducationMajor.csv', encoding='utf-16', sep='\t')
+        educationMajor.name = "EducationMajor"
+
+        minority = pd.read_csv('data/passhe_data/Minority.csv', encoding='utf-16', sep='\t')
+        minority.name = "Minority"
+
+        stem_hMajor = pd.read_csv('data/passhe_data/STEM-H_Major.csv', encoding='utf-16', sep='\t')
+        stem_hMajor.name = "STEMHMajor"
+
+        transferStudent = pd.read_csv('data/passhe_data/TransferStudent.csv', encoding='utf-16', sep='\t')
+        transferStudent.name = "TransferStudent"
+        """
+
+        data = [higherEdBySector,
+                higherEdInstitutions,
+                cost]
+        return data
 
     def merge(self):
-        data1, data2 = self.collect()
-        dataset = pd.concat([data1, data2], axis=1)
-        # Need to add how they are concatenated, filtering, etc.
+        data = self.collect()
+        for frame in data:
+            new_cols = list(frame.columns)
+            for i in range(len(new_cols)):
+                new_cols[i] = new_cols[i] + f" ({frame.name})"
+            #print(new_cols)
+            frame.rename(columns=dict(zip(frame.columns, new_cols)), inplace=True)
+        # Column merging - insitution name
+        data[0] = data[0].rename(columns={"College (HigherEducationBySector)" : "Institution"})
+        data[1] = data[1].rename(columns={"Institution Name (HigherEducationInstitutions)" : "Institution"})
+        data[2] = data[2].rename(columns={"Institution Name (Cost)" : "Institution"})
+
+        data[0] = data[0].rename(columns={"County (HigherEducationBySector)": "County"})
+        data[1] = data[1].rename(columns={"County (HigherEducationInstitutions)": "County"})
+
+        data[0] = data[0].rename(columns={"Sector (HigherEducationBySector)": "Category"})
+        data[1] = data[1].rename(columns={"Category (HigherEducationInstitutions)": "Category"})
+
+        # Column dropping
+        data[0].drop(columns=["Georeferenced Latitude & Longitude (HigherEducationBySector)", "Sector and Type (HigherEducationBySector)"], inplace=True)
+        data[1].drop(columns=["Administrative Unit Number (HigherEducationInstitutions)",
+                              "School Branch (HigherEducationInstitutions)",
+                              "Status (HigherEducationInstitutions)",
+                              "Phone Number (HigherEducationInstitutions)",
+                              "Phone_Number_Extn (HigherEducationInstitutions)",
+                              "Web_Address (HigherEducationInstitutions)",
+                              "Primary Administrator (HigherEducationInstitutions)",
+                              "School Year (HigherEducationInstitutions)"], inplace=True)
+        data[2].drop(columns=["UnitID (Cost)",
+                              "Unnamed: 9 (Cost)"], inplace=True)
+
+        # Merge
+        dataset = pd.concat(data, axis=0)
+        #print(dataset.head())
+        #print(dataset.shape)
+        dataset.to_csv("data.csv", index=False)
         return dataset
 
-    def split(self):
+    def clean_encode_split(self):
         dataset = self.merge()
-        x = dataset.drop(columns=['name of insitution col'])
-        y = dataset['name of insitution col']
-        x = LabelEncoder().fit_transform("add categorical cols here")
-        x = StandardScaler().fit_transform(x)
+        num_imputer = SimpleImputer(strategy="mean")
+        cat_imputer = SimpleImputer(strategy="constant", fill_value="Other")
+        preprocessor = ColumnTransformer([
+            ("num_imputer", num_imputer, ["zip code (HigherEducationBySector)", "Latitude (HigherEducationBySector)", "Longitude (HigherEducationBySector)", "Total price for in-district students living on campus  2024-25 (DRVCOST2024) (Cost)", "Total price for in-state students living on campus 2024-25 (DRVCOST2024) (Cost)", "Total price for in-district students living off campus (not with family)  2024-25 (DRVCOST2024) (Cost)", "Total price for in-state students living off campus (not with family)  2024-25 (DRVCOST2024) (Cost)", "Total price for in-district students living off campus (with family)  2024-25 (DRVCOST2024) (Cost)", "Total price for in-state students living off campus (with family)  2024-25 (DRVCOST2024) (Cost)"]),
+            ("cat_imputer", cat_imputer, ["Category", "Type (HigherEducationBySector)", "County", "Address (HigherEducationBySector)", "City (HigherEducationInstitutions)"])
+        ])
+        dataset["State abbreviation (HD2024) (Cost)"] = dataset["State abbreviation (HD2024) (Cost)"].fillna("PA")
+
+        le = LabelEncoder()
+        cols = ["Category", "Type (HigherEducationBySector)", "County", "Address (HigherEducationBySector)", "City (HigherEducationInstitutions)"]
+        for col in cols:
+            dataset[col] = le.fit_transform(dataset[col])
+
+        x = dataset.drop(columns=['Institution'], inplace=False)
+        y = dataset['Institution']
+        x = preprocessor.fit_transform(x.astype(object)) # Google search AI Overview
+        print(y)
         train_x, test_x, train_y, test_y = train_test_split(x, y, test_size=0.2, random_state=42)
+        return train_x, test_x, train_y, test_y
+
+    def scale(self):
+        train_x, test_x, train_y, test_y = self.clean_encode_split()
+        ss = StandardScaler()
+        train_x = ss.fit_transform(train_x)
+        test_x = ss.transform(test_x)
 
         return train_x, test_x, train_y, test_y
 
+    #
+    def nanCheck(self):
+        data = self.merge()
+        for col in data:
+            print(f"Total NaN in {col}: {data[col].isna().sum()}")
 
-data = Data()
-data.collect()
+
+
+#data = Data()
+#data.collect()
+#data.merge()
+#data.clean_split()
+#data = data.encode_scale()
+#data.nanCheck()
--- models.py
+++ models.py
@@ -6,34 +6,61 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import VotingClassifier
 from sklearn.pipeline import Pipeline
 
 class Models:
     def __init__(self):
-        d = Data()
-        data = d.split()
+        self.d = Data()
+        self.train_x, self.test_x, self.train_y, self.test_y = self.d.scale()
 
     def KNN(self):
         model = KNeighborsClassifier()
-        return model
+        pipe = Pipeline([('knn', model)])
+        pipe.fit(self.train_x, self.train_y)
+        score = pipe.score(self.test_x, self.test_y)
+        return pipe, score
 
     def DTC(self):
         model = DecisionTreeClassifier()
-        return model
+        pipe = Pipeline([('dt', model)])
+        pipe.fit(self.train_x, self.train_y)
+        score = pipe.score(self.test_x, self.test_y)
+        return pipe, score
 
     def RFC(self):
         model = RandomForestClassifier()
-        return model
+        pipe = Pipeline([('rf', model)])
+        pipe.fit(self.train_x, self.train_y)
+        score = pipe.score(self.test_x, self.test_y)
+        return pipe, score
 
     def LRC(self):
         model = LogisticRegression()
-        return model
+        pipe = Pipeline([('lr', model)])
+        pipe.fit(self.train_x, self.train_y)
+        score = pipe.score(self.test_x, self.test_y)
+        return pipe, score
 
     def gen(self):
         # Can change which models we add into the ensemble depending on the performance and data
-        KNN = self.KNN()
-        DTC = self.DTC()
-        RFC = self.RFC()
-        LRC = self.LRC()
-        #ensembleModel = Pipeline()
-        #return ensembleModel
+        KNN, score1 = self.KNN()
+        DTC, score2 = self.DTC()
+        RFC, score3 = self.RFC()
+        LRC, score4 = self.LRC()
+        print("KNN Score: ", score1)
+        print("DTC Score: ", score2)
+        print("RFC Score: ", score3)
+        print("LRC Score: ", score4)
+        ensembleModel = VotingClassifier([('KNN', KNN), ('DTC', DTC), ('RFC', RFC), ('LRC', LRC)], voting='soft')
+        return ensembleModel
+
+    def train(self, model):
+        model = self.gen()
+        print(self.train_y.shape)
+        model.fit(self.train_x, self.train_y)
+        return model.score(self.train_x, self.train_y), model.score(self.test_x, self.test_y)
+        # (0.5983791085096803, 0.3237410071942446) --> needs improvement
+
+models = Models()
+models.gen()
